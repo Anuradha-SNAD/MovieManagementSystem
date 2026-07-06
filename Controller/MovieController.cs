885b303c96d2bd31f70dff3d3b305db2600eead0
Anuradha-SNAD <anuradha.avaduthwar@snaddevelopers.com>
2026-07-06 19:57:39 +0530

    Add files via upload

---
+++ Controller/MovieController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieManagementSystem.DTOs;
+using MovieManagementSystem.Service;
+
+namespace MovieManagementSystem.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MovieController : ControllerBase
+    {
+        private readonly IMovieService service;
+
+        public MovieController(IMovieService service)
+        {
+            this.service = service;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(MovieRequestDTO dto)
+        {
+            await service.Add(dto);
+            return Ok("Movie Added Successfully");
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<MovieResponseDTO>> GetById(int id)
+        {
+           var m = await service.GetById(id);
+            return Ok(m);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<MovieResponseDTO>>> GetAll()
+        {
+            var m = await service.GetAll();
+            return Ok(m);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Upate(int id, MovieRequestDTO dto)
+        {
+            await service.Update(id, dto);
+            return Ok("Movie Updated Successfully");
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await service.Delete(id);
+            return Ok("Movie Deleted  Successfully");
+        }
+    }
+}
+ 
