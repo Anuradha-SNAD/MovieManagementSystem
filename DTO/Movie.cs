effd4d2597c835eae8f50a55e6aa6819309177e7
Anuradha-SNAD <anuradha.avaduthwar@snaddevelopers.com>
2026-07-06 19:56:36 +0530

    Add files via upload

---
+++ DTO/Movie.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieManagementSystem.Model
+{
+    public class Movie
+    {
+        [Key]
+        public int MovieId { get; set; }
+        public string Title { get; set; }
+        public decimal Budget { get; set; }
+        public decimal Collection {  get; set; }
+        public DateOnly ReleasedDate { get; set; }
+        public decimal Rating { get; set; }
+        public string Hero {  get; set; }
+        public string Heroine { get; set; }
+        public string Producer { get; set; }
+        public string Director { get; set; }
+        public string Language { get; set; }
+    }
+}
+
